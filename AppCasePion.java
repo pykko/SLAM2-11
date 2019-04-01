e459614a27d4f6fa87d34f413667ce0da0bc891b
pykko <pykko@xilim.fr>
2019-04-01 17:24:13 +0200

    Modifications Pions - Cases

--- AppCasePion.java
+++ AppCasePion.java
@@ -3,48 +3,93 @@ public class AppCasePion {
 	public static void main( String [] args ) {
 		
 		// Création de la case qui se trouve en ligne 2 et en colonne 3
+		// notée c(2,3)
+		
 		Case cs23 = new Case( 2 , 3 ) ;
 		
-		// Affichage de l'état de la case 2,3
+		
+		// Affichage de l'état (occupée ou libre) de la case c(2,3)
+		
 		if( cs23.estOccupee() == true ){
-			System.out.println( "1 - Case occupée." ) ;
+			System.out.println( "\n1 - c(2,3) est occupée." ) ;
 		}
 		else {
-			System.out.println( "1 - Case libre." ) ;
+			System.out.println( "\n1 - c(2,3) est libre." ) ;
 		}
-		// Doit afficher : 1 - Case libre.
+			// Doit afficher : 1 - c(2,3) est libre.
 		
-		// Création du pion chat rouge qui occupe la case 2,3
-		Pion pn = new Pion( 2 , "Chat" , "Rouge" , cs23 ) ;
 		
-		// Affichage du pion chat rouge
-		if( pn.estEnJeu() == true ){
-			System.out.println( "2 - Pion en jeu." ) ;
+		// Création du pion chat noir qui occupe la case c(2,3)
+		
+		Pion pnChatNoir = new Pion( 2 , "Chat" , "Noir" , cs23 ) ;
+		
+		
+		// Affichage de l'état du pion chat noir
+		
+		if( pnChatNoir.estEnJeu() == true ){
+			System.out.println( "\n2 - Le chat noir est en jeu." ) ;
 		}
 		else {
-			System.out.println( "2 - Pion capturé." ) ;
+			System.out.println( "\n2 - Le chat noir a été capturé." ) ;
 		}
-		// Doit afficher : 2 - Pion en jeu.
+			// Doit afficher : 2 - Le chat noir est en jeu
 		
 		
-		// Affichage de l'état de la case 2,3
+		// Affichage de l'état de la case c(2,3)
+		
 		if( cs23.estOccupee() == true ){
-			System.out.println( "3 - Case occupée." ) ;
+			System.out.println( "\n3 - c(2,3) est occupée." ) ;
 		}
 		else {
-			System.out.println( "3 - Case libre." ) ;
+			System.out.println( "\n3 - c(2,3) est libre." ) ;
 		}
-		// Doit afficher : 3 - Case occupée.
+			// Doit afficher : 3 - c(2,3) est occupée.
 		
 		
-		// Affichage de la ligne de la case occupée par le chat rouge
-		System.out.println( "4 - " + pn.getLaCase().getLigne() ) ;
-		// Doit afficher : 4 - 2
+		// Affichage des coordonnées de la case occupée par le chat noir
+		
+		System.out.println( "\4 - Coordonnées de la case occupée par le chat noir : " + pnChatNoir.getLaCase().getLigne() + "," + pnChatNoir.getLaCase().getColonne() ) ;
+			// Doit afficher : 4 - Coordonnées de la case occupée par le chat noir : 2,3
 		
 		
-		// Affichage du nom de l'animal qui occupe la case 2 , 3
-		System.out.println( "5 - " + cs23.getLePion().getAnimal() ) ;
-		// Doit afficher : 5 - Chat
+		// Affichage du pion qui occupe la case c(2,3)
+		
+		System.out.println( "\n5 - c(2,3) est occupée par le " + cs23.getLePion().getAnimal() + " " + cs23.getLePion().getCouleur() ) ;
+			// Doit afficher : 5 - c(2,3) est occupée par le Chat Noir
+		
+		
+		// Création de la case qui se trouve en ligne 2 et en colonne 4
+		// notée c(2,4)
+		
+		Case cs24 = new Case( 2 , 4 ) ;
+		
+		
+		// Le chat noir se déplace vers l'Est donc sur la case c(2,4)
+		
+		pnChatNoir.setLaCase( cs24 ) ;
+		
+		
+		// Affichage de l'état du pion chat noir
+		
+		if( pnChatNoir.estEnJeu() == true ){
+			System.out.println( "\n6 - Le chat noir est en jeu." ) ;
+		}
+		else {
+			System.out.println( "\n6 - Le chat noir a été capturé." ) ;
+		}
+			// Doit afficher : 6 - Le chat noir est en jeu
+			
+		
+		// Affichage de l'état de la case c(2,3)
+		
+		if( cs23.estOccupee() == true ){
+			System.out.println( "\n7 - c(2,3) est occupée." ) ;
+		}
+		else {
+			System.out.println( "\n7 - c(2,3) est libre." ) ;
+		}
+			// Doit afficher : 7 - Case occupée.
+		
 		
 		
 		
